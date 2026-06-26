d4f9c50133974a6f3bae846c26b976d2a6865d91
fmontet <koileken@gmail.com>
2026-06-26 10:51:32 +0300

    Remove controllers and services from AppModule as they are already in the imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,11 @@
 import { Module } from '@nestjs/common';
 import { InvoicesModule } from './modules/invoices/invoices.module';
-import { ReceiptsController } from './modules/receipts/receipts.controller';
-import { ReceiptsService } from './modules/receipts/receipts.service';
 import { ReceiptsModule } from './modules/receipts/receipts.module';
-import { InvoicesController } from './modules/invoices/invoices.controller';
-import { InvoicesService } from './modules/invoices/invoices.service';
-import { DocumentsService } from './modules/documents/documents.service';
 import { DocumentsModule } from './modules/documents/documents.module';
 import { AppController } from './app.controller';
 
 @Module({
   imports: [InvoicesModule, ReceiptsModule, DocumentsModule],
-  controllers: [InvoicesController, ReceiptsController, AppController],
-  providers: [InvoicesService, ReceiptsService, DocumentsService],
+  controllers: [AppController],
 })
 export class AppModule {}
